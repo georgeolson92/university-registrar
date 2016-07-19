b6a6e63fa76476f80d5493cd65235a6d916af5b4
Brittany Rausch and George Olson <brittany+george@>
2016-07-19 11:25:45 -0700

    Build nancy app to add students & courses to database and display them on the page

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,7 +9,40 @@ namespace University
   {
     public HomeModule()
     {
-      
+      Get["/"] = _ => {
+        List<Student> allStudents = Student.GetAll();
+        List<Course> allCourses = Course.GetAll();
+        Dictionary<string, object> model = new Dictionary<string, object> {};
+        model.Add("students", allStudents);
+        model.Add("courses", allCourses);
+        return View["index.cshtml", model];
+      };
+      Get["/students/new"] = _ => {
+        return View["students_form.cshtml"];
+      };
+      Post["/students/new"] = _ => {
+        Student newStudent = new Student(Request.Form["student-name"], Request.Form["student-enrollment"]);
+        newStudent.Save();
+        List<Student> allStudents = Student.GetAll();
+        List<Course> allCourses = Course.GetAll();
+        Dictionary<string, object> model = new Dictionary<string, object> {};
+        model.Add("students", allStudents);
+        model.Add("courses", allCourses);
+        return View["index.cshtml", model];
+      };
+      Get["/courses/new"] = _ => {
+        return View["courses_form.cshtml"];
+      };
+      Post["/courses/new"] = _ => {
+        Course newCourse = new Course(Request.Form["course-name"], Request.Form["course-number"]);
+        newCourse.Save();
+        List<Student> allStudents = Student.GetAll();
+        List<Course> allCourses = Course.GetAll();
+        Dictionary<string, object> model = new Dictionary<string, object> {};
+        model.Add("students", allStudents);
+        model.Add("courses", allCourses);
+        return View["index.cshtml", model];
+      };
     }
   }
 }
